ce07eb8775eacbde633466e7dc11bd531ba59468
Lucas Antonio <lucas.bialar@gmail.com>
2015-11-06 13:12:04 -0200

    Configurando datepicker

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -2,8 +2,8 @@
  * Created by Lucas on 28/10/2015.
  */
 var app = angular.module('app',[
-    'ngRoute','angular-oauth2','app.controllers','app.services','app.filters','ui.bootstrap.typeahead',
-    'ui.bootstrap.tpls'
+    'ngRoute','angular-oauth2','app.controllers','app.services','app.filters',
+    'ui.bootstrap.typeahead','ui.bootstrap.tpls', 'ui.bootstrap.datepicker'
 ]);
 
 angular.module('app.controllers',['ngMessages','angular-oauth2']);
