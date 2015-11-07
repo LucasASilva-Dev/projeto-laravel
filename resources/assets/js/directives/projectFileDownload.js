75bf0acdc0c21e771946dbc204813bc0ea82b473
Lucas Antonio <lucas.bialar@gmail.com>
2015-11-06 23:09:48 -0200

    Trabalhando com eventos

--- resources/assets/js/directives/projectFileDownload.js
+++ resources/assets/js/directives/projectFileDownload.js
@@ -3,31 +3,38 @@
  */
 angular.module('app.directives')
     .directive('projectFileDownload',
-    ['appConfig', 'ProjectFile', function(appConfig, ProjectFile){
+    ['appConfig', 'ProjectFile', '$timeout', function(appConfig, ProjectFile, $timeout){
         return {
             restrict: 'E',
             templateUrl: appConfig.baseUrl + '/build/views/templates/projectFileDownload.html',
             link: function (scope, element, attr) {
 
+                var anchor = element.children()[0];
+
+                scope.$on('salvar-arquivo', function (event, data) {
+                    $(anchor).removeClass('disabled');
+                    $(anchor).text('Save File');
+                    $(anchor).attr({
+                        href: 'data:application-octet-stream;base64,' + data.file,
+                        download: data.name
+                    });
+
+                    $timeout(function () {
+                        scope.downloadFile = function(){};
+                        $(anchor)[0].click();
+                    });
+                });
+
             },
-            controller: ['$scope', '$element', '$attrs','$timeout',
-                function ($scope, $element, $attrs, $timeout) {
+            controller: ['$scope', '$element', '$attrs',
+                function ($scope, $element, $attrs) {
                 $scope.downloadFile = function () {
+
                     var anchor = $element.children()[0];
                     $(anchor).addClass('disabled');
                     $(anchor).text('Loading...');
-                    ProjectFile.download({id: null, idFile: $attrs.idFile}, function (data) {
-                        $(anchor).removeClass('disabled');
-                        $(anchor).text('Save File');
-                        $(anchor).attr({
-                            href: 'data:application-octet-stream;base64,' + data.file,
-                            download: data.name
-                        });
-                        $timeout(function () {
-
-                            $scope.downloadFile = function(){};
-                            $(anchor)[0].click();
-                        });
+                    ProjectFile.download({id: null, idFile: $attrs.idFile}, function (data){
+                        $scope.$emit('salvar-arquivo', data);
                     })
                 };
             }]
