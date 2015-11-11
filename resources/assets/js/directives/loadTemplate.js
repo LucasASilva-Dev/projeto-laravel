6450c7088b0e5b9937c687dcdf33f046bc71f1f8
Lucas Antonio <lucas.bialar@gmail.com>
2015-11-11 20:23:20 -0200

    Testando e configurando loadTemplate

--- resources/assets/js/directives/loadTemplate.js
+++ resources/assets/js/directives/loadTemplate.js
@@ -10,13 +10,26 @@ angular.module('app.directives')
 
                 scope.$on('$routeChangeStart', function (event, next, current) {
                     if(OAuth.isAuthenticated()){
-                        $http.get(attr.url).then(function (response) {
-                            element.html(response.data);
-                            $compile(element.contents())(scope);
-                        });
+                        if(next.$$route.originalPath != '/login'
+                        && next.$$route.originalPath != '/logout') {
+                            if (!scope.isTemplateLoad) {
+                                scope.isTemplateLoad = true;
+                                $http.get(attr.url).then(function (response) {
+                                    element.html(response.data);
+                                    $compile(element.contents())(scope);
+                                });
+                            }
+                            return;
+                        }
+                    }
+
+                    resetTemplate();
+
+                    function resetTemplate(){
+                        scope.isTemplateLoad = false;
+                        element.html("");
                     }
                 });
-
             }
         };
     }]);
