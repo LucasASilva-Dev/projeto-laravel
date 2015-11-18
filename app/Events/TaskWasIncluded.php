db29badf39a284409c4e145da857346c767ec648
Lucas Antonio <lucas.bialar@gmail.com>
2015-11-18 16:45:19 -0200

    Criando evento TaskWasIncluded

---
+++ app/Events/TaskWasIncluded.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Lucas
+ * Date: 18/11/2015
+ * Time: 15:48
+ */
+namespace  CodeProject\Events;
+
+use CodeProject\Entities\ProjectTask;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Queue\SerializesModels;
+
+class TaskWasIncluded extends Event implements ShouldBroadcast{
+
+    use SerializesModels;
+
+    public $task;
+
+    public function __construct(ProjectTask $task)
+    {
+        $this->task = $task;
+    }
+
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return array
+     */
+    public function broadcastOn()
+    {
+        return ['user.'.\Authorizer::getResourceOwnerId()];
+    }
+}
