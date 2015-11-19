41fec68ec25be85427b50fcdadfb8aebe55464ad
Lucas Antonio <lucas.bialar@gmail.com>
2015-11-19 02:03:31 -0200

    Ajustando autocomplete de client

--- resources/assets/js/controllers/project/projectEdit.js
+++ resources/assets/js/controllers/project/projectEdit.js
@@ -3,8 +3,8 @@
  */
 angular.module('app.controllers')
     .controller('ProjectEditController',
-    ['$scope', '$routeParams', '$location', "$cookies",  'Project', 'Client', 'appConfig',
-        function ($scope, $routeParams, $location, $cookies, Project, Client, appConfig){
+    ['$scope', '$routeParams', '$location', "$cookies", '$q', '$filter', 'Project', 'Client', 'appConfig',
+        function ($scope, $routeParams, $location, $cookies, $q, $filter, Project, Client, appConfig){
 
             Project.get({id: $routeParams.id}, function (data) {
                 $scope.project = data;
@@ -40,10 +40,18 @@ angular.module('app.controllers')
             };
 
             $scope.getClients = function (name) {
-                return Client.query({
+                var deffered = $q.defer();
+                Client.query({
                     search: name,
                     searchFields: 'name:like'
-                }).$promise;
+                }, function (data) {
+                    var result = $filter('limitTo')(data.data,10);
+                    deffered.resolve(result);
+                }, function(error){
+                    deffered.reject(error);
+                });
+                return deffered.promise;
+
             };
 
             $scope.selectClient = function (item){
