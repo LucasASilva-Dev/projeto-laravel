a4b8748e8fe1b84937630c7658371937ccb18565
Lucas Antonio <lucas.bialar@gmail.com>
2015-11-19 01:05:24 -0200

    Configurando real-time do painel de project

--- public/build/js/controllers/home.js
+++ public/build/js/controllers/home.js
@@ -2,8 +2,19 @@
  * Created by Lucas on 28/10/2015.
  */
 angular.module('app.controllers')
-    .controller('HomeController', ['$scope', '$cookies', function ($scope, $cookies){
+    .controller('HomeController', ['$scope', '$cookies', '$pusher', '$timeout', function ($scope, $cookies, $pusher, $timeout){
 
-        //$cookies.get('nomedoCookie);
-        console.log($cookies.getObject('user').email);
+        $scope.tasks = [];
+        s
+        var pusher = $pusher(window.client);
+        var channel = pusher.subscribe('user.'+$cookies.getObject('user').id);
+        channel.bind('CodeProject\\Events\\TaskWasIncluded',
+            function(data) {
+                if($scope.tasks.length == 6) {
+                    $scope.tasks.splice($scope.tasks.length - 1, 1);
+                }
+                $timeout(function () {
+                    $scope.tasks.unshift(data.task);
+                }, 300);
+            });
     }]);
